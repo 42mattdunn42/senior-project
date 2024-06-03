999da7eadadd9b470e20b493df4275b7999878dd
dunnm10atwit <dunnm10@wit.edu>
2024-06-03 13:21:48 -0400

    added numbers to health bars

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider healthBar;
+    public TextMeshProUGUI text;
     public Player player;
     public Enemy enemy;
     public bool isPlayerBar;
     private void Start()
     {
         healthBar = GetComponent<Slider>();
+        //text =  GetComponent<TextMeshProUGUI>();
         if (isPlayerBar) {
             healthBar.maxValue = player.maxhp;
             healthBar.value = player.maxhp;
-            Debug.Log(healthBar.maxValue);
+            text.text = player.maxhp.ToString();
         }
         else
         {
             healthBar.maxValue = enemy.maxhp;
             healthBar.value = enemy.maxhp;
+            text.text = enemy.maxhp.ToString();
         }
         
     }
@@ -28,10 +32,12 @@ public class HealthBar : MonoBehaviour
         if (isPlayerBar)
         {
             healthBar.value = player.hp;
+            text.text = player.hp.ToString();
         }
         else
         {
             healthBar.value = enemy.hp;
+            text.text = enemy.hp.ToString();
         }
     }
 }
